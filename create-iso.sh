4bf396fc507ee29ef78ec7ca288fb8462d6cadfe
Steffen Froemer <steffen@redhat.com>
2016-12-02 05:02:14 +0100

    initial commit

---
+++ create-iso.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+ then echo "Please run as root"
+ exit
+fi
+
+pushd `dirname $0` > /dev/null
+BASE=$(pwd -P)
+popd > /dev/null
+
+ISO=/home/sfroemer/Downloads/rhel-server-7.3-x86_64-boot.iso
+OUT_ISO=${BASE}/rhel-7-rescue.iso
+
+RHEL7RESCUE="${BASE}/build/rhel-7-rescue"
+initrd="${BASE}/build/initrd"
+mnt="${BASE}/build/mnt"
+
+mkdir -p ${RHEL7RESCUE}
+mkdir -p ${initrd}
+mkdir -p ${mnt}
+
+mount -t iso9660 -o ro ${ISO} ${mnt}
+
+# copy files from boot-iso
+shopt -s dotglob
+cp -avRf ${mnt}/* ${RHEL7RESCUE}
+
+#unmount dvd iso
+umount ${mnt}
+
+#replace isolinux.cfg 
+cp -af ${BASE}/isolinux.cfg ${RHEL7RESCUE}/isolinux/isolinux.cfg
+
+# extract initrd files
+cd ${initrd}
+xz -dc < ${RHEL7RESCUE}/isolinux/initrd.img | cpio --quiet -i --make-directories
+cd -
+
+# add coreutils to initrd
+for file in $(rpm -ql coreutils); do mkdir -p ${initrd}$(dirname ${file}); cp -af $file ${initrd}${file}; done
+
+# add xfs-check script to initrd
+cp -a ${BASE}/scripts/* ${initrd}
+# add systemd-unit file for running xfs-chk on startup
+# replace dracut-emergency
+cp -a ${BASE}/dracut-emergency ${initrd}/bin/dracut-emergency
+
+# rebuild initrd
+cd ${initrd}
+find . 2>/dev/null | cpio --quiet -c -o | xz -9 --format=lzma >"${RHEL7RESCUE}/isolinux/initrd.img"
+cd -
+
+# create rescue-iso
+cd ${rhel-7-rescue}
+mkisofs -J -T -o ${OUT_ISO} -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -graft-points -V "RHEL-7.3 x86_64" ${RHEL7RESCUE}
+cd -
+
+# cleanup
+sudo rm -rf $BASE/build
---
+++ scripts/1-check-only.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+
+## We need to check all vd-devices
+
+# check all xfs-filesystems
+list_xfs_fs () {
+  blkid | grep xfs | cut -d":" -f1 | grep vd
+}
+
+
+# check volume-groups
+list_lvm_devices () {
+  blkid | grep LVM2_member | cut -d":" -f1 | grep vd
+}
+
+list_lvm_vgs () {
+    for PV in $(list_lvm_devices); do
+    lvm pvs | grep ${PV} | awk -F" " '{print $2}'
+  done
+}
+
+activate_lvm_vg () {
+  for VG in $(list_lvm_vgs); do
+    lvm vgchange -ay ${VG}
+  done
+
+}
+
+list_lvm_lvs () {
+  for VG in $(list_lvm_vgs); do
+    for LV in $(lvm lvs | grep ${VG} | grep -v swap | awk -F" " '{print $1}'); do
+      echo "/dev/mapper/${VG}-${LV}"
+    done
+  done
+}
+
+# check if all files are unmounted
+check_mounted () {
+  device=$1
+  mount | grep ${device} >/dev/null; echo $?
+}
+
+mount_cycle () {
+  DEV=$1
+  if [ 0 -eq $(check_mounted ${DEV}) ]; then
+    umount ${DEV}
+  fi
+  if [ ! -d /mnt ]; then
+    mkdir /mnt
+  fi
+  if [ 0 -eq $(mount ${device} /mnt >/dev/null; echo $?) ]; then
+    umount ${device}
+  else
+    exit 1
+  fi
+}
+
+repair_device () {
+  device=$1
+  mount_cycle ${device}
+  dev_name=${device##/dev*/}
+  if [ $? -eq 0 ]; then 
+    xfs_repair -n ${device} > xfs-check.${dev_name}.log 2>&1; echo $?
+  fi
+}
+
+
+# get boot disk for root filesystem
+#rootvg=$(lvm pvs | grep vda2 | awk -F" " '{print $2}')
+
+# activate volume-groups
+#lvm vgchange -ay ${rootvg}
+
+# 1. repair all xfs volumes, which are no LVM-member
+# 2. 
+ERROR_FOUND=0
+
+for DEV in $(list_xfs_fs); do 
+  if [ 0 -ne $(repair_device ${DEV}) ]; then
+    ERROR_FOUND=1
+    break
+  fi
+done
+
+activate_lvm_vg
+
+for DEV in $(list_lvm_lvs); do
+  if [ 0 -ne $(repair_device ${DEV}) ]; then
+    ERROR_FOUND=1
+    break
+  fi
+done
+
+if [ 0 -eq $ERROR_FOUND ]; then 
+  #_emergency_action=halt
+  #exit 1
+  systemctl isolate poweroff.target
+else
+  echo " ##############################################################"
+  echo " # XFS-ERROR detected. There is one drive with XFS-errors     #"
+  echo " # Please run 'xfs_repair' manually on all your drives        #"
+  echo " ##############################################################"
+  exec sh -i -l
+fi
+
+exit 0
